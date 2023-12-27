a677766316713a7bd5b3e559c41d867e41e73cc0
Lars S <>
2023-12-26 21:04:05 -0800

    v0.0.1 First commit, I hope this inspires some players.

---
+++ NPC.py
@@ -0,0 +1,205 @@
+#!/bin/python3
+"""
+This script reads from a series of tables and assembles an NPC from them.
+
+If you make changes to the default stats dict in render_stats you can balance 
+this according to the power of the players that you have.
+"""
+from random import choice, sample
+from itertools import cycle
+
+
+def read_table(filename: str, count: int = 1):
+    """This just gets a random line from a given file in the tables dir."""
+    with open(f"tables/{filename}.txt", "r", encoding="UTF-8") as tmpfile:
+        lines = [line.strip() for line in tmpfile.readlines()]
+    if count == 1:
+        return choice(lines)
+    else:
+        return " ".join(sample(lines, count))
+
+
+class NPC:
+    """A neat object for containing all if an NPC's info. You could easily import this into
+    a larger project that needs NPCs!"""
+
+    def __init__(self, level: int = 1):
+        """First name uses gender to determine which list of names to use.
+        If the gender is other then it randomly picks between male/female names.
+        """
+        self.level: int = level
+        self.gender = read_table("genders")
+        self.first_name = first_name_gen(self)
+        self.last_name = read_table("family")
+        self.race = read_table("race")
+        self.job = read_table("jobs")
+        self.stats = render_stats(self)
+
+        self.hair = read_table("hair")
+        self.eyes = read_table("eyes")
+        self.skin = read_table("skin_tone")
+        self.quirks = read_table("quirks", 2)
+        self.motiv = read_table("motivations")
+
+    def __repr__(self) -> str:
+        """This allows you to cleanly write the NPC to the console or a file."""
+        return f"{self.first_name} {self.last_name}, {self.gender}, {self.race}/{self.job}, Lv. {self.level}.\n{self.hair} hair, {self.eyes} eyes, {self.skin} skin.\n{self.stats}\nQuirks: {self.quirks}\nMotivation: {self.motiv}"
+
+
+def first_name_gen(C: NPC):
+    if C.gender == "other":
+        name_gender = choice(["male", "female"])
+    else:
+        name_gender = C.gender
+    return read_table(name_gender)
+
+
+def dice(x: int = 20):
+    """This can be any dice in the game, and can easily be used for hp or attack rolls.
+    x defaults to 20 for ease of use but I recommend using real dice for role-play and digital
+    dice for mass generation.
+    """
+    if x < 2:
+        print("The smallest dice you can roll is two sided but you tried {x}.")
+        return 0
+    return choice(range(1, x + 1))
+
+
+def modify(stat: int):
+    """This takes any stat value and returns it's modifier. You may also do this in your
+    head but Python needs the formula.
+    """
+    return (stat - 10) // 2
+
+
+def calc_hp(level: int, con: int, hit: int):
+    """This is the standard DND HP formula per level. This can be used
+    to calculate fully legal player character HP stats.
+
+    All you need is the characters level, constitution and hit dice.
+    """
+    mod = modify(con)
+    hp = mod + hit
+    if level == 1:
+        return hp
+
+    for _ in range(level - 1):
+        hp += mod + dice(hit)
+    return hp
+
+
+# C stands for character in this function, I didn't want to confuse it
+# with "cha" which stands for charisma.
+def render_stats(C: NPC):
+    """
+    This one function does a lot and holds many internal dicts. But it allows
+    us to fully calculate the stats of an NPC at any level with any race/class combo.
+
+    You have to track race/class abilities yourself. Things like dark-vision are not
+    documented here but the books should have that info.
+
+    You can modify a lot by simply changing the starting stats dict at the top of the func because
+    all the other calculations are additive. You can even set someone to negative starting
+    stats for some reason.
+
+    hit refers to the hit dice for a given NPC throughout this function.
+    """
+
+    # Base stats, adjust as needed.
+    stats: dict = {
+        "hp": 3,
+        "dc": 11,
+        "spd": 5,
+        "str": 8,
+        "int": 8,
+        "dex": 8,
+        "wis": 8,
+        "con": 8,
+        "cha": 8,
+        "hit": 0,
+    }
+
+    # These are the movement speed and stat values given for each race.
+    race_bonuses: dict = {
+        "Dragonborn": [("spd", 30), ("str", 2), ("cha", 1)],
+        "Dwarf": [("spd", 25), ("con", 2)],
+        "Elf": [("spd", 30), ("dex", 2)],
+        "Gnome": [("spd", 25), ("int", 2)],
+        "Halfling": [("spd", 25), ("dex", 2)],
+        "Half-Elf": [("spd", 30), ("cha", 2)],
+        "Half-Orc": [("spd", 30), ("str", 2), ("con", 1)],
+        "Human": [("spd", 30)],
+        "Tiefling": [("spd", 30), ("int", 1), ("cha", 2)],
+    }
+
+    # This is six points focused on the given classes preferred stats.
+    # This helps the random NPC have a little focus without min-maxing.
+    # And it tracks HP hit dice too!
+    class_bonuses: dict = {
+        "Barbarian": [("con", 4), ("str", 2), ("hit", 12)],
+        "Bard": [("cha", 4), ("int", 2), ("hit", 8)],
+        "Cleric": [("wis", 4), ("cha", 2), ("hit", 8)],
+        "Druid": [("wis", 4), ("dex", 2), ("hit", 8)],
+        "Fighter": [("str", 4), ("con", 2), ("hit", 10)],
+        "Monk": [("dex", 4), ("wis", 2), ("hit", 8)],
+        "Paladin": [("cha", 4), ("str", 2), ("hit", 10)],
+        "Ranger": [("str", 4), ("dex", 2), ("hit", 10)],
+        "Rogue": [("dex", 4), ("cha", 2), ("hit", 8)],
+        "Sorcerer": [("con", 4), ("int", 2), ("hit", 6)],
+        "Warlock": [("int", 4), ("con", 2), ("hit", 8)],
+        "Wizard": [("int", 4), ("wis", 2), ("hit", 6)],
+    }
+
+    # These lines add race bonuses
+    race = race_bonuses[C.race]
+    for pair in race:
+        key, val = pair[0], pair[1]
+        stats[key] += val
+
+    # These lines add class bonuses
+    cls = class_bonuses[C.job]
+    for pair in cls:
+        key, val = pair[0], pair[1]
+        stats[key] += val
+
+    # The stat pool starts at 21 because the class already spent six points. There is an additional
+    # point per level beyond level 1.
+    pool: int = 21 + ((C.level - 1) * 1)
+    count = 0
+    # This cycle just puts one point in each stat until the pool is empty.
+    for stat in cycle(["str", "int", "dex", "wis", "con", "cha"]):
+        if pool < 1:
+            break
+
+        # Tried to stop NPCs from putting too many points into any one stat at level 1.
+        if stats[stat] < 18 + C.level // 4:
+            stats[stat] += 1
+            pool -= 1
+        else:
+            count += 1
+
+        # This stops infinite looping when all stats are maxed and the pool isn't empty.
+        if count > 1000:
+            break
+
+    # This is fully DND legal HP.
+    stats["hp"] += calc_hp(C.level, stats["con"], stats["hit"])
+
+    # Maybe add the level // 4 or something.
+    stats["dc"] += modify(stats["dex"]) + (C.level // 4)
+
+    res = ", ".join([f"{k} {v}" for k, v in stats.items()])
+    return res
+
+
+if __name__ == "__main__":
+    # No need for argv is this is being used as a module!
+    from sys import argv
+
+    # This cleverly handles the IndexError and the ValueError with the same default value.
+    try:
+        level = int(argv[1])
+    except:
+        level = 1
+
+    print(NPC(level))
---
+++ README.txt
@@ -0,0 +1,68 @@
+This program combines a bunch of text files representing DM tables and uses them to generate custom
+NPCs or level 1 player characters. My DM guide is still in the mail so I apologize for any incorrect
+stats those will be fixed soon.
+
+I did apply a few stat buffs beyond level 1 for the NPCs because they won't have all the cool class/race
+abilities the player characters have.
+
+You may use your own tables as drop in replacements for everything except jobs.txt and race.txt because
+those tables have hardcoded bonuses. I will explain how to expand that part of the code in another document.
+
+To use simply run:
+$ python3 NPC.py
+
+Without any arguments a level one character will be created. You can also give a single integer as an argument:
+$ python3 NPC.py 20
+
+To generate a character at that level.
+
+Note that this program needs to be in the same directory as the tables/ directory or nothing will happen.
+
+---
+
+For my private games I am using some custom tables not provided in this repo. I do not have the rights
+to publish those tables so I provided some very short example tables instead. 
+
+I do have the ability to link you to the custom tables I used and you can do with those links what you wish!
+
+TABLE LINKS!
+I do not in any way vouch for the security or safety of these websites. Always use an ad-blocker
+and HTTPs when visiting random websites.
+
+Female Names:
+https://www.cs.cmu.edu/Groups/AI/areas/nlp/corpora/names/female.txt
+
+
+Male Names:
+https://www.cs.cmu.edu/Groups/AI/areas/nlp/corpora/names/male.txt
+
+
+Family Names:
+The family names list is unsorted, and contains duplicates of every name. As well as names that
+don't contain any vowels which are useless in English. You can adjust that list with a few
+simple commands in Bash/Python. Good luck, and God Speed!
+
+https://www.cs.cmu.edu/Groups/AI/areas/nlp/corpora/names/other/family.txt
+
+
+Quirks:
+There are over 200 quirks on this link and a few of them are repetitive or don't seem fun to role-play. I just read 
+through the list and picked out my favorite 70 or so. This should get you started and you can easily add more as you 
+think of them.
+
+https://kindlepreneur.com/character-quirks/
+
+
+Motivations:
+This list is shorter than the quirks and I had no desire to modify it. These are 30 very well written prompts!
+
+https://www.writerswrite.co.za/30-character-motivations-to-kickstart-your-story/
+
+
+Hair, Eyes, Skin-tone:
+This link has almost the exact same table for all three attributes. I liked their hair table enough to use it and I
+recommend you copy that table yourself. I did need more creative/expressive colors for eyes and skin-tones and so I
+have provided the full tables that I wrote after doing a fair amount of research. Feel free to use the provided 
+eyes/skin-tones tables for any MIT license compatible purposes.
+
+https://www.dandwiki.com/wiki/Random_Hair_and_Eye_Color_(DnD_Other)
